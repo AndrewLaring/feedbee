70be20f65577300f0ecb1ed9878ac1997bde0783
AndrewLaring <larinoffski@gmail.com>
2016-11-23 05:46:07 +0200

    pull with laring_1

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('BASEPATH', __DIR__ . '/');
-define('URL', 'http://bee.tt/');
-
-define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'beejee');
-define('DB_USER', 'root');
-define('DB_PASS', '23');
