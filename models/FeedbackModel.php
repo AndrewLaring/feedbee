f946f1982bc38175e90441ca0cb82c8da9bb60c9
AndrewLaring <larinoffski@gmail.com>
2016-11-24 05:15:09 +0200

    first trevis commit

--- models/FeedbackModel.php
+++ models/FeedbackModel.php
@@ -4,14 +4,30 @@ namespace app\models;
 
 use laring\core\Model;
 
+/**
+ * Class FeedbackModel
+ * @package app\models
+ */
 class FeedbackModel extends Model
 {
 
+    /**
+     * @var
+     */
+    public $feedback;
+
+    /**
+     * FeedbackModel constructor.
+     */
     public function __construct()
     {
         parent::__construct();
     }
 
+    /**
+     * @param $sort_by
+     * @return array
+     */
     public function getFeedbacks($sort_by)
     {
         $st = $this->db->prepare("SELECT * FROM fb WHERE STATUS in(1, 3) ORDER BY $sort_by DESC");
@@ -20,6 +36,10 @@ class FeedbackModel extends Model
         return $feedbacks;
     }
 
+    /**
+     * @param $sKey
+     * @return array
+     */
     public function getAllFeedbacks($sKey)
     {
         $st = $this->db->prepare("SELECT * FROM fb ORDER BY $sKey DESC");
@@ -28,6 +48,10 @@ class FeedbackModel extends Model
         return $feedbacks;
     }
 
+    /**
+     * @param $params
+     * @return string
+     */
     public function createFeedback($params)
     {
         $st = $this->db->prepare("INSERT into fb (ID, NAME, EMAIL, MESSAGE, IMAGE, STATUS) VALUES (NULL, ?, ?, ?, ?, 0)");
@@ -42,6 +66,9 @@ class FeedbackModel extends Model
         }
     }
 
+    /**
+     * @param $data
+     */
     public function validate($data)
     {
 
--- views/_layouts/_index.php
+++ views/_layouts/_index.php
@@ -7,7 +7,8 @@
     <title>BEE JEE</title>
     <link href="<?= URL ?>images/main/ninja.png" rel="shortcut icon" type="image/png"/>
     <link rel="stylesheet" href="<?= URL ?>css/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="<?= URL ?>css/style.css">
+<!--    <link rel="stylesheet" href="--><?//= URL ?><!--css/style.css">-->
+    <link rel="stylesheet" href="<?= URL ?>css/trevis.css">
     <!--    <link rel="stylesheet" href="--><? //= URL ?><!--css/new_style.css">-->
     <script type="text/javascript" src="<?= URL ?>js/jquery-2.1.4.js"></script>
     <script type="text/javascript" src="<?= URL ?>js/bootstrap/bootstrap.min.js"></script>
